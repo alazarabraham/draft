29b2e802686abe538cb83dc34eae23b5d0d715ab
alazarabraham <amarkos@xpanxion.com>
2020-08-24 14:28:57 -0400

    first commit

---
+++ myscript.js
@@ -0,0 +1,13 @@
+$(() => {
+    $('a[href*="#"]:not([href="#"])').click((e) => {
+        const target = $(e.target.hash);
+
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+
+            return false;
+        }
+    });
+});
